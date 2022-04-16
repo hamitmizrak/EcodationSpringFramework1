ea7ed77d90926ea38c8d4ea8c6377cdaa57aa994
hamitmizrak <hamitmizrak@gmail.com>
2022-04-16 12:48:21 +0300

    @stereotype revize

--- src/main/java/com/hamitmizrak/stereotypesx/StereoTypeBean.java
+++ src/main/java/com/hamitmizrak/stereotypesx/StereoTypeBean.java
@@ -1,5 +1,13 @@
 package com.hamitmizrak.stereotypesx;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//lombok
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 //eclipse netbeans sts intellij
 @BenimStereoTypeim
