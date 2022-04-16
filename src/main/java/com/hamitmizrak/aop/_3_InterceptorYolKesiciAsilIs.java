1e55b6e2db131f6bffaa549d44716aef51088bf9
hamitmizrak <hamitmizrak@gmail.com>
2022-04-16 11:45:48 +0300

    AOP @Interceptor revize tamam

--- src/main/java/com/hamitmizrak/aop/_3_InterceptorYolKesiciAsilIs.java
+++ src/main/java/com/hamitmizrak/aop/_3_InterceptorYolKesiciAsilIs.java
@@ -1,52 +1,61 @@
 package com.hamitmizrak.aop;
 
-
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
 import javax.swing.*;
-import java.util.Scanner;
 
 @InterceptorKesici
 @Interceptor
 public class _3_InterceptorYolKesiciAsilIs {
 
     //database arama yapacak
-    public boolean databaseSearch(String userEmail,String userPassword){
+    public boolean databaseSearch() {
+        System.setProperty("java.awt.headless", "false"); //Disables headless
         //default email: root@gmail.com
         //default şifre: root
-        if(  (userEmail.equals("root@gmail.com"))  && (userPassword.equals("root"))  )
-           return false;
+        String userEmail, userPassword;
+        userEmail = JOptionPane.showInputDialog("Lütfen email giriniz");
+        userPassword = JOptionPane.showInputDialog("Lütfen password giriniz");
+
+        if ((userEmail.equals("root@gmail.com")) && (userPassword.equals("root")))
+            return true;
         else
             System.out.println("Email veya Şifre yanlıştır");
-            return true;
+        return false;
     }
 
     @AroundInvoke
     public Object isLogin(InvocationContext context) {
 
         //database böyle bir kullanıcı var mı yok mu ?
-        //kullanıcı sisteme kayıtlı değilse ==> boolean isLogin=true;
-        //kullanıcı sisteme kayıtlı ise     ==> boolean isLogin=false;
+        //kullanıcı sisteme kayıtlı değilse ==> boolean isLogin=false;
+        //kullanıcı sisteme kayıtlı ise     ==> boolean isLogin=true;
+        boolean isLogin = databaseSearch();
+        int counter =1;
+        while (isLogin == false) {
+            System.out.println("Hata giriş sayısı: "+counter);
+            isLogin = databaseSearch();
+            counter++;
+        }
 
-        //kullanıcı sisteme kayıtlı ise false
-        String email="root44@gmail.com",password="root";
-        boolean isLogin=databaseSearch(email,password);
 
         //Database kullanıcı varsa devam etsin yoksa sisteme giriş yapamazsınız yazsın
-        Object isContinue=null;
+        Object isContinue = null;
 
         //Kullanıcı olup olmamasına göre devam edecek
-        if(isLogin){
+        if (!isLogin) {
             System.out.println("!!! Dikkat Öncelikle Sisteme kayıt olmalısınız");
             return null;
-        }else{
+        } else {
             try {
-                isContinue=  context.proceed(); //devam etsin
-                System.out.println("Method bilgisi: "+context.getMethod().getName());
+                System.out.println("*******************************");
                 System.out.println("Hoşgeldiniz Sayın : Ferhat");
-                System.out.println("Sisteme Giriş yapıldı. "+isContinue);
-            }catch(Exception e){
+                System.out.println("Hata giriş sayısı: "+counter);
+                isContinue = context.proceed(); //devam etsin
+                System.out.println("Method bilgisi: " + context.getMethod().getName());
+                System.out.println("Sisteme Giriş yapıldı. " + isContinue);
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
