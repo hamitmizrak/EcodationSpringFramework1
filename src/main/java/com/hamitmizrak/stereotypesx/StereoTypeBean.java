b0008c9e2813435388d33bea3943d899d0c10065
hamitmizrak <hamitmizrak@gmail.com>
2022-04-16 12:18:22 +0300

    @stereotype

---
+++ src/main/java/com/hamitmizrak/stereotypesx/StereoTypeBean.java
@@ -0,0 +1,8 @@
+package com.hamitmizrak.stereotypesx;
+
+
+//eclipse netbeans sts intellij
+@BenimStereoTypeim
+public class StereoTypeBean {
+    private String adi="ben stereotype bean'im";
+}
---
+++ src/main/java/com/hamitmizrak/stereotypesx/StereoTypeNedir.java
@@ -0,0 +1,7 @@
+package com.hamitmizrak.stereotypesx;
+
+public class StereoTypeNedir {
+
+    //StereoTypes: zamanla anotation fazlalışır sürekli eklemek zorunda kalırız.
+    //eğer sürekli aynı anontation eklemek zorunda isek biz istersek stereotypes kullanabiliriz
+}
